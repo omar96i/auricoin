169021092d519d13fed601b65e49db6db3609630
omar96i <sagara9930@hotmail.com>
2024-01-06 14:00:38 -0500

    update

--- resources/views/letter_credit_memory/index.blade.php
+++ resources/views/letter_credit_memory/index.blade.php
@@ -159,7 +159,7 @@
                 <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Carta de credito</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Memoria</button>
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Swift</button>
             </li>
         </ul>
         <div class="tab-content" id="myTabContent">
